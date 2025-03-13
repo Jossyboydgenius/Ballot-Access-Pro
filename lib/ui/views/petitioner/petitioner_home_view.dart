92d20f91ad56a8a0369dd13f2cec80ce913fce80
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-13 17:09:28 +0000

    Add PetitionerHomeView widget to serve as the main interface for petitioners, featuring a bottom navigation bar to switch between Map, Leads, Reports, and Profile views. This enhances user navigation and interaction within the application.

---
+++ lib/ui/views/petitioner/petitioner_home_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ballot_access_pro/shared/constants/app_colors.dart';
+import 'package:ballot_access_pro/shared/styles/app_text_style.dart';
+import 'package:ballot_access_pro/shared/utils/app_sizer.dart';
+import 'package:ballot_access_pro/ui/views/petitioner/map_view.dart';
+import 'package:ballot_access_pro/ui/views/petitioner/leads_view.dart';
+import 'package:ballot_access_pro/ui/views/petitioner/reports_view.dart';
+import 'package:ballot_access_pro/ui/views/petitioner/profile_view.dart';
+
+class PetitionerHomeView extends StatefulWidget {
+  const PetitionerHomeView({super.key});
+
+  @override
+  State<PetitionerHomeView> createState() => _PetitionerHomeViewState();
+}
+
+class _PetitionerHomeViewState extends State<PetitionerHomeView> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const MapView(),
+    const LeadsView(),
+    const ReportsView(),
+    const ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    AppDimension.init(context);
+    
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            activeIcon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline),
+            activeIcon: Icon(Icons.people),
+            label: 'Leads',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_outlined),
+            activeIcon: Icon(Icons.bar_chart),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+} 
