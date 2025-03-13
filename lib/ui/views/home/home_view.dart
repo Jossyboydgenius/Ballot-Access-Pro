9a0e4b37cb0ea59dcb4e0eef4773fbe8d627995f
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-13 16:15:43 +0000

    Add HomeView widget with bottom navigation for seamless access to Map, Leads, Reports, and Profile tabs, enhancing user experience in the application.

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ballot_access_pro/shared/constants/app_colors.dart';
+import 'package:ballot_access_pro/shared/styles/app_text_style.dart';
+import 'package:ballot_access_pro/ui/views/home/tabs/map_view.dart';
+import 'package:ballot_access_pro/ui/views/home/tabs/leads_view.dart';
+import 'package:ballot_access_pro/ui/views/home/tabs/reports_view.dart';
+import 'package:ballot_access_pro/ui/views/home/tabs/profile_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const MapView(),
+    const LeadsView(),
+    const ReportsView(),
+    const ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          selectedItemColor: AppColors.primary,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: AppTextStyle.regular12,
+          unselectedLabelStyle: AppTextStyle.regular12,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map_outlined),
+              activeIcon: Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline),
+              activeIcon: Icon(Icons.people),
+              label: 'Leads',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart_outlined),
+              activeIcon: Icon(Icons.bar_chart),
+              label: 'Reports',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+} 
