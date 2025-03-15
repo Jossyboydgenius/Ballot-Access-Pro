c435f44eacb541a45cb67f6caa910c1cc1494a85
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-15 06:18:08 +0000

    Add LocalStorageService for secure storage management and authentication handling

---
+++ lib/services/local_storage_service.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../core/locator.dart';
+
+class LocalStorageKeys {
+  static String refreshToken = 'refreshToken';
+  static String accessToken = 'accessToken';
+  static String expiresIn = 'expiresIn';
+  static String debugName = 'debugName';
+  static String debugEmail = 'debugEmail';
+  static String debugPassword = 'debugPassword';
+  static String showedOnboarding = 'showedOnboarding';
+  static String timedOut = 'timedOut';
+  static String useBiometric = 'useBiometric';
+}
+
+class LocalStorageService {
+  final fSStorage = locator<FlutterSecureStorage>();
+
+  Future<String?> getStorageValue(String key) async {
+    String? value = await fSStorage.read(key: key);
+    return value;
+  }
+
+  Future<void> saveStorageValue(String key, String value) async {
+    return await fSStorage.write(key: key, value: value);
+  }
+
+  Future<void> clearAuthAll() async {
+    await fSStorage.delete(key: LocalStorageKeys.accessToken);
+    await fSStorage.delete(key: LocalStorageKeys.refreshToken);
+    await fSStorage.delete(key: LocalStorageKeys.expiresIn);
+    await fSStorage.delete(key: LocalStorageKeys.debugName);
+    await fSStorage.delete(key: LocalStorageKeys.debugEmail);
+    await fSStorage.delete(key: LocalStorageKeys.debugPassword);
+    await fSStorage.delete(key: LocalStorageKeys.expiresIn);
+    await fSStorage.delete(key: LocalStorageKeys.timedOut);
+  }
+
+  Future<void> clearAll() async => await fSStorage.deleteAll();
+} 
