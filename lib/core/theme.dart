56beb8be48d18b10eb714bddb8b4d2bd72e610bf
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-15 06:17:52 +0000

    Add AppTheme class for defining light theme styles and configurations

---
+++ lib/core/theme.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+        ),
+        bodyMedium: TextStyle(
+          color: Colors.black,
+          fontSize: 14,
+        ),
+        titleLarge: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.blue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+} 
