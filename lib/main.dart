7833d6c9641d9ef1c0f0bb7a309ef09f1e6ec114
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-15 06:17:00 +0000

    Refactor main function for async initialization and update app structure with environment configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ballot_access_pro/shared/navigation/app_routes.dart';
-import 'package:ballot_access_pro/shared/navigation/navigation_service.dart';
-import 'package:ballot_access_pro/core/theme/app_theme.dart';
+import 'dart:async';
 
-void main() {
+import 'package:ballot_access_pro/core/flavor_config.dart';
+import 'package:ballot_access_pro/shared/navigation/navigation_service.dart';
+import 'package:ballot_access_pro/shared/widgets/widgets.dart';
+import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/locator.dart';
+import 'shared/navigation/app_routes.dart';
+import 'core/theme/app_theme.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+
+  await dotenv.load(fileName: ".env");
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
+  await setUpLocator(AppFlavorConfig(
+    name: 'Ballot Access Pro',
+    apiBaseUrl: dotenv.env['BASE_URL_PROD']!,
+    webUrl: dotenv.env['WEB_URL_PROD']!,
+    sentryDsn: dotenv.env['SENTRY_DSN']!,
+    mixpanelToken: dotenv.env['MIXPANEL_TOKEN_PROD']!,
+  ));
+
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +47,19 @@ class MyApp extends StatelessWidget {
           title: 'Ballot Access Pro',
           debugShowCheckedModeBanner: false,
           theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: ThemeMode.system,
           navigatorKey: NavigationService.navigatorKey,
           initialRoute: AppRoutes.initialRoute,
           routes: AppRoutes.routes,
+          builder: (context, child) {
+            child = ConnectionWidget(
+              dismissOfflineBanner: false,
+              builder: (context, isOnline) {
+                return BotToastInit()(context, child);
+              },
+            );
+            return child;
+          },
+          navigatorObservers: [BotToastNavigatorObserver()],
         );
       },
     );
