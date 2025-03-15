c5b55df9731b67878f4c025089a66634c3e25ea4
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-15 06:39:43 +0000

    Register additional plugins for Linux and Windows platforms

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,8 +6,14 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_remote_config_web/firebase_remote_config_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:permission_handler_html/permission_handler_html.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -16,8 +22,14 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseRemoteConfigWeb.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   GoogleMapsPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   WebPermissionHandler.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
