1e3926c692a4daa29abb3ee7a63c9733e6712894
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-16 10:50:03 +0000

    Update application namespace and bundle identifiers to reflect new package structure; add MainActivity for Android integration.

---
+++ android/app/src/main/kotlin/com/ballotaccesspro/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ballotaccesspro.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
+
--- android/app/src/main/kotlin/com/ballotaccesspro/ballot_access_pro/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ballotaccesspro.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
