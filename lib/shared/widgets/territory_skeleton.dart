2ee9bdbb82f017d1573ab369f2061303127fcaaa
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-16 07:16:19 +0000

    Add TerritorySkeleton widget to display loading placeholders with Shimmer effect, improving user experience during data fetching.

---
+++ lib/shared/widgets/territory_skeleton.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TerritorySkeleton extends StatelessWidget {
+  const TerritorySkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildShimmerContainer(120.w, 24.h),
+          SizedBox(height: 16.h),
+          Container(
+            padding: EdgeInsets.all(16.w),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.r),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 8,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      _buildShimmerContainer(60.w, 24.h),
+                      const Spacer(),
+                      _buildShimmerContainer(120.w, 16.h),
+                    ],
+                  ),
+                  SizedBox(height: 16.h),
+                  _buildShimmerContainer(100.w, 20.h),
+                  SizedBox(height: 16.h),
+                  ...List.generate(
+                    3,
+                    (index) => Padding(
+                      padding: EdgeInsets.only(bottom: 8.h),
+                      child: _buildShimmerContainer(double.infinity, 16.h),
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+                  ...List.generate(
+                    3,
+                    (index) => Padding(
+                      padding: EdgeInsets.only(bottom: 8.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildShimmerContainer(80.w, 16.h),
+                          _buildShimmerContainer(120.w, 16.h),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildShimmerContainer(double width, double height) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4.r),
+      ),
+    );
+  }
+} 
