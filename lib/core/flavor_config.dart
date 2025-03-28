eaf2bab728e7ba020829c203c1b25b278304e9b8
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-28 12:12:11 +0000

    Add socket URL property to AppFlavorConfig for enhanced real-time communication support.

--- lib/core/flavor_config.dart
+++ lib/core/flavor_config.dart
@@ -1,6 +1,7 @@
 class AppFlavorConfig {
   final String name;
   final String apiBaseUrl;
+  final String socketUrl;
   final String webUrl;
   final String sentryDsn;
   final String mixpanelToken;
@@ -8,6 +9,7 @@ class AppFlavorConfig {
   AppFlavorConfig({
     required this.name,
     required this.apiBaseUrl,
+    required this.socketUrl,
     required this.webUrl,
     required this.sentryDsn,
     required this.mixpanelToken,
