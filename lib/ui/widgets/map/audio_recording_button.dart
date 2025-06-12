2b76f1deed6dcbdb0e322a6f7519ea8f19179c7a
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-12 17:05:29 +0000

    Add ignore directive for synchronous context in audio recording button widget. This change improves code quality by preventing potential issues related to context usage during asynchronous operations.

--- lib/ui/widgets/map/audio_recording_button.dart
+++ lib/ui/widgets/map/audio_recording_button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ballot_access_pro/shared/constants/app_colors.dart';
